f691fd8f65e85f7793629e431d58c2efea27fbd0
theleadingzero <becky@theleadingzero.com>
2013-11-18 18:52:44 +0000

    Adding Arduino code to etch-a-sketch.

---
+++ etch_a_sketch/lesson3/etch_x_arduino/etch_x_arduino.ino
@@ -0,0 +1,19 @@
+// variables for pin
+int xPin = A0;
+
+
+void setup() {
+  // start Serial
+  Serial.begin( 9600 );
+}
+
+void loop() {
+  // read in value
+  int xValue = analogRead( xPin );
+  
+  // print out value
+  Serial.println( xValue );
+  
+  // wait a little bit
+  delay( 30 );
+}
